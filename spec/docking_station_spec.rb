597c438ca25ba998ac16daf8ae86099dbc0d5a43
“rogrenke” <roland@rolandgrenke.com>
2017-05-09 11:58:48 +0100

    write unit for method release_bike, add method to DockingStation class

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,9 @@
 require 'docking_station'
 
 describe DockingStation do
+  # describe DockingStation.respond_to?(:release_bike) do
+  #   it {is_expected.to eq true}
+  # end
+  it {is_expected.to respond_to :release_bike}
 
 end
