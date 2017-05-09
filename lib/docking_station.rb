0363f29f2e28a2797aa5585f87aa45b08629899d
“rogrenke” <roland@rolandgrenke.com>
2017-05-09 16:01:57 +0100

    Challenge 11 - write feature test for docking a bike and unit test for dock_bike method. Update docking_station.rb to pass tests.  Add instance variable to show if bike is docked

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,16 @@
 require '/Users/ro/Documents/03Learning2Code/Makers/boris_bikes/lib/bike.rb'
 
 class DockingStation
+  attr_reader :bike_docked
+  def initialize
+    @bike_docked = false
+  end
+
   def release_bike
-    new_bike = Bike.new
+    Bike.new
+  end
+
+  def dock_bike(bike)
+    @bike_docked = true
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,7 +2,18 @@ require 'docking_station'
 
 describe DockingStation do
   it {is_expected.to respond_to :release_bike}
-  it "checks if it gets a bike" do
-    expect(DockingStation.new.release_bike.working?).to be_truthy
+  it "checks if it gets a working bike" do
+    expect(DockingStation.new.release_bike.working?).to eq true
+  end
+  it "allows a bike to be docked" do
+    station = DockingStation.new
+    station.dock_bike(Bike.new)
+    expect(station.bike_docked).to eq true
+  end
+  it "Has a method to make docking a bike possible" do
+    expect(DockingStation.new.respond_to?("dock_bike")).to eq true
+  end
+  it "Uses an attr_reader to check if a bike is docked" do
+    expect(DockingStation.new.respond_to?("bike_docked")).to eq true
   end
 end
