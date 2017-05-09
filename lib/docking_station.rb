e558d3e05ff867357e5ab65beaa4497ed8562d45
“rogrenke” <roland@rolandgrenke.com>
2017-05-09 12:48:52 +0100

    write unit test getting a bike and checking if its working, update docking_station.rb and bike.rb to pass the test

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,4 +1,7 @@
+require '/Users/ro/Documents/03Learning2Code/Makers/boris_bikes/lib/bike.rb'
+
 class DockingStation
   def release_bike
+    new_bike = Bike.new
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,9 +1,8 @@
 require 'docking_station'
 
 describe DockingStation do
-  # describe DockingStation.respond_to?(:release_bike) do
-  #   it {is_expected.to eq true}
-  # end
   it {is_expected.to respond_to :release_bike}
-
+  it "checks if it gets a bike" do
+    expect(DockingStation.new.release_bike.working?).to be_truthy
+  end
 end
