d5d107b8c19e4d322e533ade32181281aeb2a2f5
Kynosaur <kye.bracey@gmail.com>
2017-05-10 22:58:48 +0100

    Reset everything to just after completing challenge 12

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,7 @@
-require '/Users/ro/Documents/03Learning2Code/Makers/boris_bikes/lib/bike.rb'
+require_relative 'bike'
 
 class DockingStation
-  attr_reader :bike_docked
+  attr_reader :bike, :bike_docked
   def initialize
     @bike_docked = false
   end
@@ -9,10 +9,11 @@ class DockingStation
   def release_bike
     raise "No bike available!" unless @bike_docked
     @bike_docked = false
-    Bike.new
+    @bike
   end
 
   def dock_bike(bike)
+    @bike = bike
     @bike_docked = true
   end
 end
